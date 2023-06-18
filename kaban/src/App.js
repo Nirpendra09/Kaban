776eee6953ff918c8af2a2c07b4447682723d8e5
Nirpendra Chaudhary <nirpendra09@gmail.com>
2023-06-18 11:38:13 +0000

    Kanban system WIP

---
+++ kaban/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Kanban from './components/kanban';
+
+function App() {
+    return (
+        <div className="App">
+            <Kanban />
+        </div>
+    );
+}
+
+export default App;
